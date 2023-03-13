79f746116efeab68f2614fe672344285c9e0b9c6
Jonas Blum <jonasblum5@gmail.com>
2023-03-13 20:21:56 +0100

    Made change to main.yml about sonarquabe

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
@@ -42,7 +42,7 @@ public class UserService {
   public User createUser(User newUser) {
     newUser.setToken(UUID.randomUUID().toString());
     newUser.setStatus(UserStatus.OFFLINE);
-    checkIfUserExists(newUser);
+    //checkIfUserExists(newUser);
     // saves the given entity but data is only persisted in the database once
     // flush() is called
     newUser = userRepository.save(newUser);
