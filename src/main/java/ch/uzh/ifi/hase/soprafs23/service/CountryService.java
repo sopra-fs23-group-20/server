7dbdb9c76a81dfef6a7530d4c07b03e5389a07cf
robonder <jonasblum5@gmail.com>
2023-03-28 12:04:37 +0200

    Changed gitignore

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/CountryService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/CountryService.java
@@ -51,11 +51,21 @@ private final OutlineRepository outlineRepository;
         return randomCountryId;
     }
 
-/*
-    public void setCountriesWithAPI() {
 
-        try {
-            // Connect to the REST API URL and read the JSON response
+    private String getCountryOutline(String countryCode, JSONArray outlines){
+
+        for (Object featureObj : outlines) {
+            JSONObject feature = (JSONObject) featureObj;
+            JSONObject properties = (JSONObject) feature.get("properties");
+            if (countryCode.equals(properties.get("ISO_A3"))) {
+                return  featureObj.toString();
+            }
+        }
+        return null;
+    }
+
+    public JSONArray fetchCountriesAPI(JSONArray countries) throws IOException, ParseException{
+
             URL url = new URL("https://restcountries.com/v3.1/all");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
@@ -70,63 +80,16 @@ private final OutlineRepository outlineRepository;
 
             // Parse the JSON response and extract the name and population of each country
             JSONParser parser = new JSONParser();
-            JSONArray countries = (JSONArray) parser.parse(response);
-            for (Object obj : countries) {
-                JSONObject country = (JSONObject) obj;
-                String name = (String) ((JSONObject) country.get("name")).get("common");
-                long population = (long) country.get("population");
-                String flag = (String) ((JSONObject) country.get("flags")).get("svg");
-                List<Double> latlng = (List<Double>) country.get("latlng");
-
-
-                Country newCountry = new Country();
-                newCountry.setName(name);
-                newCountry.setPopulation(population);
-                newCountry.setFlag(flag);
-                newCountry.setLatitude(latlng.get(0));
-                newCountry.setLongitude(latlng.get(1));
-                countryRepository.save(newCountry);
-            }
-            countryRepository.flush();
-        }
-        catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
+            countries = (JSONArray) parser.parse(response);
+            System.out.println("Set countries with API");
+        return countries;
     }
 
- */
-
-    private String getCountryOutline(String countryCode, JSONArray outlines){
-
-        for (Object featureObj : outlines) {
-            JSONObject feature = (JSONObject) featureObj;
-            JSONObject properties = (JSONObject) feature.get("properties");
-            if (countryCode.equals(properties.get("ISO_A3"))) {
-                return  featureObj.toString();
-            }
-        }
-        return null;
-    }
     public void setAllCountries() {
         if (countryRepository.getAllCountryIds().size() == 0) {
             JSONArray countries = null;
             try{
-                URL url = new URL("https://restcountries.com/v3.1/all");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
-                InputStream stream = connection.getInputStream();
-                Scanner scanner = new Scanner(stream);
-                StringBuilder responseBuilder = new StringBuilder();
-                while (scanner.hasNext()) {
-                    responseBuilder.append(scanner.nextLine());
-                }
-                String response = responseBuilder.toString();
-
-                // Parse the JSON response and extract the name and population of each country
-                JSONParser parser = new JSONParser();
-                countries = (JSONArray) parser.parse(response);
-                System.out.println("Set countries with API");
+                countries = fetchCountriesAPI(countries);
             }
             catch (IOException | ParseException e) {
                 JSONParser parser = new JSONParser();
