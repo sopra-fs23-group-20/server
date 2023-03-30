1a72431b51aa366ce7975898eb847136a1b20b3b
JonathanContrerasM <jonathan.contreras.michael@gmail.com>
2023-03-30 13:34:56 +0200

    Added all possible test for the user repo... in form of UserRepositoryIntegrationTests

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
@@ -9,8 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
@@ -43,4 +42,80 @@ public class UserRepositoryIntegrationTest {
         assertEquals(found.getToken(), user.getToken());
         assertEquals(found.getStatus(), user.getStatus());
     }
+
+    @Test
+    public void findByUserId_success() {
+        // given
+        User user = new User();
+        user.setPassword("testPswd");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setCreation_date(new Date());
+        user.setToken("1");
+
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // when
+        User found = userRepository.findByUserId(user.getUserId());
+
+        // then
+        assertNotNull(found);
+        assertEquals(found.getUserId(), user.getUserId());
+        assertEquals(found.getUsername(), user.getUsername());
+        assertEquals(found.getToken(), user.getToken());
+        assertEquals(found.getStatus(), user.getStatus());
+    }
+
+    @Test
+    public void findByToken_success() {
+        // given
+        User user = new User();
+        user.setPassword("testPswd");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setCreation_date(new Date());
+        user.setToken("1");
+
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // when
+        User found = userRepository.findByToken(user.getToken());
+
+        // then
+        assertNotNull(found);
+        assertEquals(found.getUserId(), user.getUserId());
+        assertEquals(found.getUsername(), user.getUsername());
+        assertEquals(found.getToken(), user.getToken());
+        assertEquals(found.getStatus(), user.getStatus());
+    }
+
+    @Test
+    public void findByUsername_notFound() {
+        // when
+        User found = userRepository.findByUsername("nonExistentUsername");
+
+        // then
+        assertNull(found);
+    }
+
+    @Test
+    public void findByUserId_notFound() {
+        // when
+        User found = userRepository.findByUserId(-1L);
+
+        // then
+        assertNull(found);
+    }
+
+    @Test
+    public void findByToken_notFound() {
+        // when
+        User found = userRepository.findByToken("nonExistentToken");
+
+        // then
+        assertNull(found);
+    }
+
 }
