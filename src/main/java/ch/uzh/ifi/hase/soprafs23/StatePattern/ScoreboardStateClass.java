9936da44cb2f1cabc12a0e45c5ea70697e7e0719
robonder <jonasblum5@gmail.com>
2023-04-06 14:26:16 +0200

    refactoring

--- src/main/java/ch/uzh/ifi/hase/soprafs23/StatePattern/ScoreboardStateClass.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/StatePattern/ScoreboardStateClass.java
@@ -1,12 +1,23 @@
 package ch.uzh.ifi.hase.soprafs23.StatePattern;
 
+import ch.uzh.ifi.hase.soprafs23.constant.GameState;
+import ch.uzh.ifi.hase.soprafs23.constant.WebsocketType;
 import ch.uzh.ifi.hase.soprafs23.entityDB.Game;
 import ch.uzh.ifi.hase.soprafs23.service.GameService;
 
 public class ScoreboardStateClass implements GameStateClass{
     @Override
     public Game updateGameEverySecond(Game game, GameService gameService) {
-        if(game.getRemainingRounds() > 0)
+        if(game.getRemainingRounds() == 0){
+            game.setCurrentState(GameState.ENDED);
+            return game;
+        }
+        if(game.getRemainingTime()>0) {
+            game.setRemainingTime(game.getRemainingTime() - 1);
+            gameService.updateGameState(game.getGameId(), WebsocketType.TIMEUPDATE, game.getRemainingTime());
+            return game;
+        }
+        game.setRemainingTime(game.get);
         return game;
     }
 }
