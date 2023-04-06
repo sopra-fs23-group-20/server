5271744f65685e798071738cbddee67fe81c0af4
Jonas Blum <jonasblum5@gmail.com>
2023-04-06 21:32:15 +0200

    Fixed Bug that after Scoreboard, Setup would be shown quickly

--- src/main/java/ch/uzh/ifi/hase/soprafs23/StatePattern/ScoreboardStateClass.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/StatePattern/ScoreboardStateClass.java
@@ -5,6 +5,8 @@ import ch.uzh.ifi.hase.soprafs23.constant.WebsocketType;
 import ch.uzh.ifi.hase.soprafs23.entityDB.Game;
 import ch.uzh.ifi.hase.soprafs23.service.GameService;
 
+import java.util.*;
+
 public class ScoreboardStateClass implements GameStateClass{
     @Override
     public Game updateGameEverySecond(Game game, GameService gameService) {
@@ -17,12 +19,27 @@ public class ScoreboardStateClass implements GameStateClass{
             else {
                 //Let SETUP State handle the rest
                 game.setRemainingRounds(game.getRemainingRounds() - 1);
-                game.setCurrentState(GameState.SETUP);
+                game.setCurrentState(GameState.GUESSING);
+                game.setRemainingRoundPoints(100L);
+                Set<Long> allCountriesIds = game.getCountriesToPlayIds();
+                selectNewRandomCountry(game);
+                game.setRemainingTime(game.getRoundDuration());
+                gameService.updateGameState(game.getGameId(), WebsocketType.GAMESTATEUPDATE, game.getCurrentState());
+                gameService.updateGameState(game.getGameId(), WebsocketType.TIMEUPDATE, game.getRemainingTime());
             }
             return game;
         }
         game.setRemainingTime(game.getRemainingTime() - 1);
         gameService.updateGameState(game.getGameId(), WebsocketType.TIMEUPDATE, game.getRemainingTime());
         return game;
+    }
+
+    private void selectNewRandomCountry(Game game) {
+        List<Long> myList = new ArrayList<>(game.getCountriesToPlayIds());
+        Random random = new Random();
+        int randomIndex = random.nextInt(myList.size());
+        game.setCurrentCountryId(myList.get(randomIndex));
+        myList.remove(randomIndex);
+        game.setCountriesToPlayIds(new HashSet<>(myList));
     }
 }
